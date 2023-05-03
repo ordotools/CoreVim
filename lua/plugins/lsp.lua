33e6adf122df697db16a12f4e8831e8027c66ba3
corei8 <corei8.github@gmail.com>
2023-05-03 11:34:50 -0400

    allow for tab completion for lsp completion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,20 +32,74 @@ return {
 
     local cmp = require('cmp')
     local cmp_action = require('lsp-zero').cmp_action()
+    local luasnip = require('luasnip')
 
     cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
+      },
+
+      completion = {
+        completeopt = 'menu,menuone,noselect',
+      },
+
       mapping = {
-        -- `Enter` key to confirm completion
-        ['<CR>'] = cmp.mapping.confirm({select = false}),
-
-        -- Ctrl+Space to trigger completion menu
-        ['<C-Space>'] = cmp.mapping.complete(),
-
-        -- Navigate between snippet placeholder
-        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-      }
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+        ['<C-e>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+          }),
+        ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+        ['<Tab>'] = function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end,
+        ['<S-Tab>'] = function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end,
+      },
+      sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' }, -- For luasnip users.
+        }, {
+          { name = 'buffer' },
+        })
     })
+    -- cmp.setup({
+    --   mapping = {
+    --     -- `Enter` key to confirm completion
+    --     ['<CR>'] = cmp.mapping.confirm({select = false}),
+    --     -- ['<Shift-Tab>'] = cmp.mapping.scroll_docs(4),
+    --     -- ['<Tab>'] = cmp.mapping.scroll_docs(-4),
+    --     -- Ctrl+Space to trigger completion menu
+    --     ['<C-Space>'] = cmp.mapping.complete(),
+
+    --     -- Navigate between snippet placeholder
+    --     ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    --     ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+    --   }
+    -- })
   end,
 },
 }
