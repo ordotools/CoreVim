f95dce232a6dfda7e4301d54b1381ea0357b153a
corei8 <38426287+corei8@users.noreply.github.com>
2023-06-15 23:38:30 -0400

    update configuration for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,15 @@ return {
     'nvim-telescope/telescope.nvim',
     -- branch = '0.1.1',
     version = false,
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function ()
+      require('telescope').setup{
+        pickers = {
+          find_files = {
+            theme = "dropdown",
+          }
+        },
+      }
+    end
   },
 }
