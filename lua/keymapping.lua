b9bc6a71baa679cd4e32c02d43dc54c473a1fb6a
corei8 <38426287+corei8@users.noreply.github.com>
2023-08-05 14:43:05 -0400

    get rid of whichkey

--- lua/keymapping.lua
+++ lua/keymapping.lua
@@ -1,9 +1,31 @@
--- just a few things that have not made it into which-key
-
 vim.cmd([[
 
 inoremap jj <ESC>
-"noremap <leader>s :%s//g<left><left>
+
+" Search
+noremap <leader>s :%s//g<left><left>
+
+" Telescope
+nmap <leader>ff Telescope find_files<cr>
+nmap <leader>fg Telescope live_grep<cr>
+nmap <leader>fh Telescope help_tags<cr>
+nmap <leader>fb Telescope buffers<cr>
+nmap <leader>ft TodoTelescope keywords=TODO,FIX<cr>
+
+" Git
+nmap <leader>gd diffget //3<cr>
+noremap <leader>gc G commit -m ''<left>
+nmap <leader>gp G push<cr>
+nmap <leader>gt G push --tag<cr>
+nmap <leader>gl G pull<cr>
+
+" Gist
+vmap <leader>gii GistCreate<cr>
+nmap <leader>gif GistCreateFromFile<cr>
+nmap <leader>gil GistList<cr>
+
+" Todo
+nmap <leader>t TodoQuickFix<cr>
 
 " LaTeX bindings
 vmap <leader>xb c\textbf{<C-r>"}<ESC>
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,63 +1,63 @@
 return {
 
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
-    config = function ()
+--   {
+--     "folke/which-key.nvim",
+--     event = "VeryLazy",
+--     init = function()
+--       vim.o.timeout = true
+--       vim.o.timeoutlen = 300
+--     end,
+--     config = function ()
 
-      local wk = require("which-key")
+--       local wk = require("which-key")
 
-      wk.register({
+--       wk.register({
 
-        f = {
-          name = "  Search",
-          f = {"<cmd>Telescope find_files<cr>", "Find File"},
-          g = {"<cmd>Telescope live_grep<cr>", "Live Grep"},
-          h = {"<cmd>Telescope help_tags<cr>", "Search Help Tags"},
-          b = {"<cmd>Telescope buffers<cr>", "Search Buffers"},
-          t = {"<cmd>TodoTelescope keywords=TODO,FIX<cr>", "Search TODOs"},
-        },
+--         f = {
+--           name = "  Search",
+--           f = {"<cmd>Telescope find_files<cr>", "Find File"},
+--           g = {"<cmd>Telescope live_grep<cr>", "Live Grep"},
+--           h = {"<cmd>Telescope help_tags<cr>", "Search Help Tags"},
+--           b = {"<cmd>Telescope buffers<cr>", "Search Buffers"},
+--           t = {"<cmd>TodoTelescope keywords=TODO,FIX<cr>", "Search TODOs"},
+--         },
 
-        g = {
-          name = "  Git",
-          -- mode = "nv",
-          d = {":diffget //3<cr>", "Diff"},
-          -- a = {":G add .<cr>", "Add"},
-          c = {":G commit -m ''<left>", "Commit", noremap=true},
-          p = {":G push<cr>", "Push"},
-          l = {":G pull<cr>", "Pull"},
-          s = {":G<cr>", "Command"},
+--         g = {
+--           name = "  Git",
+--           -- mode = "nv",
+--           d = {":diffget //3<cr>", "Diff"},
+--           -- a = {":G add .<cr>", "Add"},
+--           c = {":G commit -m ''<left>", "Commit", noremap=true},
+--           p = {":G push<cr>", "Push"},
+--           l = {":G pull<cr>", "Pull"},
+--           s = {":G<cr>", "Command"},
 
-          i = {
-            name = "  Gist",
-            i = {"<cmd>GistCreate<cr>", "Gist from Selection", mode="v"},
-            f = {"<cmd>GistCreateFromFile<cr>", "Gist from File", mode="n"},
-            l = {"<cmd>GistsList<cr>", "Gist List", mode="n"},
-          },
+--           i = {
+--             name = "  Gist",
+--             i = {"<cmd>GistCreate<cr>", "Gist from Selection", mode="v"},
+--             f = {"<cmd>GistCreateFromFile<cr>", "Gist from File", mode="n"},
+--             l = {"<cmd>GistsList<cr>", "Gist List", mode="n"},
+--           },
 
-        },
+--         },
 
-        t = {"<cmd>TodoQuickFix<cr>", "TODO Quick Fix"},
+--         t = {"<cmd>TodoQuickFix<cr>", "TODO Quick Fix"},
 
-        x = {
-          name = "  LaTeX",
-          b = {'c\textbf{<cr>"}<esc>', "Bold", noremap=false},
-          i = {'c\textbf{<cr>"}<esc>', "Italic", noremap=false}
-        },
+--         x = {
+--           name = "  LaTeX",
+--           b = {'c\textbf{<cr>"}<esc>', "Bold", noremap=false},
+--           i = {'c\textbf{<cr>"}<esc>', "Italic", noremap=false}
+--         },
 
-        s = {":%s//g<left><left>", "Search & Replace in File", noremap=true},
+--         s = {":%s//g<left><left>", "Search & Replace in File", noremap=true},
 
-      },{ -- opts
+--       },{ -- opts
 
-          prefix = "<leader>"
+--           prefix = "<leader>"
 
-        })
+--         })
 
-    end,
-  }
+--     end,
+--   }
 
 }
