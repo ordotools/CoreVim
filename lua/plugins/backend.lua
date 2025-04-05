1f87a7d58fcd470c384567e65c1f7fce9fe7cdfc
corei8 <38426287+corei8@users.noreply.github.com>
2025-04-05 08:08:29 -0400

    adjust some colorscheme things

--- lua/plugins/backend.lua
+++ lua/plugins/backend.lua
@@ -1,5 +1,16 @@
 return {
 
+    -- FIX: we are importing two sets of icons...
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    },
+
     {
         'echasnovski/mini.nvim',
         version = false,
